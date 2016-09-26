be8efcd9800859ac06a869211086ab773aa2d509
binhex <megalith01@gmail.com>
2016-09-26 16:35:18 +0100

    fixed order for par2 multi core

--- setup/root/aur.sh
+++ setup/root/aur.sh
@@ -23,14 +23,11 @@ su -c "tar -xvf $aur_helper.tar.gz" - makepkg-user
 su -c "cd /home/makepkg-user/$aur_helper && makepkg -s --noconfirm --needed" - makepkg-user
 pacman -U /home/makepkg-user/$aur_helper/packer*.pkg.tar.xz --noconfirm
 
+# install app using aur helper (seperate due to conflict with par2cmdline
+su -c "$aur_helper -S par2cmdline-tbb --noconfirm" - makepkg-user
+
 # install app using aur helper
 su -c "$aur_helper -S $aur_packages --noconfirm" - makepkg-user
-
-# remove single threaded par2 (dependency of sabnzbd)
-pacman -Rs par2cmdline --noconfirm
-
-# install par2 multithreaded first using packer (cannot bundle with sabnzbd as order means arch single threaded par2 gets installed first)
-su -c "$aur_helper -S par2cmdline-tbb --noconfirm" - makepkg-user
 
 # remove base devel excluding useful core packages
 pacman -Ru $(pacman -Qgq base-devel | grep -v pacman | grep -v sed | grep -v grep | grep -v gzip) --noconfirm
