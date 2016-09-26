cff4005d1621ed502fe5a8df232e8383e02cfe12
binhex <megalith01@gmail.com>
2016-09-26 13:56:05 +0100

    include multi-thread par2

--- setup/root/aur.sh
+++ setup/root/aur.sh
@@ -7,7 +7,7 @@ set -e
 aur_helper="packer"
 
 # define aur packages
-aur_packages="sabnzbd"
+aur_packages="par2cmdline-tbb sabnzbd"
 
 # create "makepkg-user" user for makepkg
 useradd -m -s /bin/bash makepkg-user
