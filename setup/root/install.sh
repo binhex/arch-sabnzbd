f49ca4c1b100d53e121e43a8607e55285cbcdd26
binhex <megalith01@gmail.com>
2016-11-09 22:10:48 +0000

    update base, consolidate scripts

--- setup/root/install.sh
+++ setup/root/install.sh
@@ -3,14 +3,70 @@
 # exit script if return code != 0
 set -e
 
+# build scripts
+####
+
+# download build scripts from github
+curl -o /tmp/scripts-master.zip -L https://github.com/binhex/scripts/archive/master.zip
+
+# unzip build scripts
+unzip /tmp/scripts-master.zip -d /tmp
+
+# move shell scripts to /root
+find /tmp/scripts-master/ -type f -name '*.sh' -exec mv -i {} /root/  \;
+
+# pacman packages
+####
+
 # define pacman packages
 pacman_packages="base-devel git python2-pyopenssl python2-feedparser p7zip"
 
-# install pre-reqs
-pacman -S --needed $pacman_packages --noconfirm
+# install compiled packages using pacman
+if [[ ! -z "${pacman_packages}" ]]; then
+	pacman -S --needed $pacman_packages --noconfirm
+fi
 
-# call aur script (arch user repo)
+# aor packages
+####
+
+# define arch official repo (aor) packages
+aor_packages=""
+
+# call aor script (arch official repo)
+source /root/aor.sh
+
+# aur packages
+####
+
+# define aur helper
+aur_helper="apacman"
+
+# define aur packages
+aur_packages="par2cmdline-tbb sabnzbd"
+
+# call aur install script (arch user repo)
 source /root/aur.sh
+
+# container perms
+####
+
+# create file with contets of here doc
+cat <<'EOF' > /tmp/permissions_heredoc
+# set permissions inside container
+chown -R "${PUID}":"${PGID}" /usr/lib/couchpotato /usr/bin/couchpotato /home/nobody
+chmod -R 775 /usr/lib/couchpotato /usr/bin/couchpotato /home/nobody
+
+EOF
+
+# replace permissions placeholder string with contents of file (here doc)
+sed -i '/# PERMISSIONS_PLACEHOLDER/{
+    s/# PERMISSIONS_PLACEHOLDER//g
+    r /tmp/permissions_heredoc
+}' /root/init.sh
+rm /tmp/permissions_heredoc
+
+# env vars
+####
 
 # cleanup
 yes|pacman -Scc
