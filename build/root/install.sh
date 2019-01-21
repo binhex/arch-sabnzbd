13eb985a1b2a795fe64ee84b8f11829a13bfa069
binhex <megalith01@gmail.com>
2019-01-21 13:05:02 +0000

    inc python3, req for nzbtomedia script

--- build/root/install.sh
+++ build/root/install.sh
@@ -19,7 +19,7 @@ mv /tmp/scripts-master/shell/arch/docker/*.sh /root/
 ####
 
 # define pacman packages
-pacman_packages="git python2 python2-pyopenssl python2-feedparser p7zip"
+pacman_packages="git python2 python2-pyopenssl python2-feedparser p7zip python3"
 
 # install compiled packages using pacman
 if [[ ! -z "${pacman_packages}" ]]; then
