4408b8e5b59105cb7e8a4d7643fb7cb480505857
binhex <megalith01@gmail.com>
2022-08-22 22:20:33 +0100

    switch to virtualenv

--- run/nobody/start.sh
+++ run/nobody/start.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/dumb-init /bin/bash
 
+install_path="/usr/lib/sabnzbd"
+virtualenv_path="${install_path}/venv"
+
+# activate virtualenv where requirements have been installed from install.sh
+source "${virtualenv_path}/bin/activate"
+
 # run app
-python3 /usr/lib/sabnzbd/SABnzbd.py --config-file /config --server 0.0.0.0:8080 --https 8090
+python3 "${install_path}/SABnzbd.py" --config-file /config --server 0.0.0.0:8080 --https 8090
