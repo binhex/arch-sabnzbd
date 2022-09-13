5e7e98f8a01afe16fea5a8f90904076894cf82e5
binhex <megalith01@gmail.com>
2022-09-13 22:26:09 +0100

    add comment to indicate fg

--- run/nobody/start.sh
+++ run/nobody/start.sh
@@ -6,5 +6,5 @@ virtualenv_path="${install_path}/venv"
 # activate virtualenv where requirements have been installed from install.sh
 source "${virtualenv_path}/bin/activate"
 
-# run app
+# run app (foreground)
 python3 "${install_path}/SABnzbd.py" --config-file /config --server 0.0.0.0:8080 --https 8090
